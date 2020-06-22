cfad45b23ee29a8a0ac6cc2db1e91ee002c07135
exodious <exodious@users.noreply.github.com>
2020-06-21 21:30:34 -0400

    Update example on_boot.sh for directory

---
+++ on-boot-script/examples/udm-files/on_boot.d/10-wpa_supplicant.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## create files like this with different numbers for execution order
+## ala /etc/profile.d
+
+## example command to run, please replace with your own.
+podman start wpa_supplicant-udmpro
