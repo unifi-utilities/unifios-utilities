4c33ec26d996f404c318085e55877ffae9934646
exodious <exodious@users.noreply.github.com>
2020-06-21 21:26:22 -0400

    Store install-cni-plugins.sh once

--- cni-plugins/install-cni-plugins.sh
+++ cni-plugins/install-cni-plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 curl -L https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-arm64-v0.8.6.tgz -o /tmp/cni.tgz
 mkdir -p /mnt/data/podman/cni/
 tar xf /tmp/cni.tgz -C /mnt/data/podman/cni/
