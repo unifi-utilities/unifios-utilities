1f980e53559fb6fdb79154a6f32398019bd6e074
exodious <exodious@users.noreply.github.com>
2020-06-21 21:28:31 -0400

    Newlines in on-boot-script

--- on-boot-script/install.sh
+++ on-boot-script/install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 podman cp install-unifios.sh unifi-os:/root/install-unifios.sh
 podman exec unifi-os chmod +x /root/install-unifios.sh
 podman exec unifi-os sh -c /root/install-unifios.sh
