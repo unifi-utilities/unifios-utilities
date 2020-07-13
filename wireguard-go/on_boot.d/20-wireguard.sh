09e7bcd55bd86377cd17bee0892afc7592105a5e
John Dorman <dorman@ataxia.cloud>
2020-07-12 22:54:09 -0700

    Clarifying wireguard based on feedback

--- wireguard-go/on_boot.d/20-wireguard.sh
+++ wireguard-go/on_boot.d/20-wireguard.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-podman run -i -d --rm --net=host --name wireguard --privileged -v /mnt/data/wireguard:/etc/wireguard -v /dev/net/tun:/dev/net/tun  -e LOG_LEVEL=info -e WG_COLOR_MODE=always masipcat/wireguard-go
+# Starts a wireguard container that is deleted after it is stopped.
+# All configs stored in /mnt/data/wireguard
+
+podman run -i -d --rm --net=host --name wireguard --privileged \
+    -v /mnt/data/wireguard:/etc/wireguard \
+    -v /dev/net/tun:/dev/net/tun \
+    -e LOG_LEVEL=info -e WG_COLOR_MODE=always \
+    masipcat/wireguard-go
