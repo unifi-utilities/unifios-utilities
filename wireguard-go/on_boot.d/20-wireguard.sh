0116cf8c000aac3e2bc2411edba726822b300f56
John D <427295+boostchicken@users.noreply.github.com>
2021-04-05 02:43:35 -0700

    Update 20-wireguard.sh

--- wireguard-go/on_boot.d/20-wireguard.sh
+++ wireguard-go/on_boot.d/20-wireguard.sh
@@ -9,6 +9,6 @@ else
     -v /mnt/data/wireguard:/etc/wireguard \
     -v /dev/net/tun:/dev/net/tun \
     -e LOG_LEVEL=info -e WG_COLOR_MODE=always \
-    masipcat/wireguard-go
+    masipcat/wireguard-go:latest-arm64v8
 fi
 
