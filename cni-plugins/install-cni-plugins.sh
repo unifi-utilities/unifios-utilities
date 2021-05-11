5a453e66946859867b43bff73c225ebbddec520f
Adam Bolsover <13262235+bowseruk@users.noreply.github.com>
2021-05-11 22:45:57 +0100

    Update install-cni-plugins.sh
    Updated cni plugin version to later version to resolve clan issues with some containers.

--- cni-plugins/install-cni-plugins.sh
+++ cni-plugins/install-cni-plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-arm64-v0.9.0.tgz -o /tmp/cni.tgz
+curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-arm64-v0.9.1.tgz -o /tmp/cni.tgz
 mkdir -p /mnt/data/podman/cni/
 tar xf /tmp/cni.tgz -C /mnt/data/podman/cni/
 rm /tmp/cni.tgz
