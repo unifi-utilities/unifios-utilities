884d8a36c5c97e099f241f52b489c56265d2996b
Adam Bolsover <13262235+bowseruk@users.noreply.github.com>
2021-05-11 22:48:11 +0100

    Update 25-homebridge.sh
    Updated cni plugin version to resolve clan issues.

--- homebridge/on_boot.d/25-homebridge.sh
+++ homebridge/on_boot.d/25-homebridge.sh
@@ -5,7 +5,7 @@ CONTAINER=homebridge
 CNI_PATH=/mnt/data/podman/cni
 if [ ! -f "$CNI_PATH"/tuning ]; then
     mkdir -p $CNI_PATH
-    curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-arm64-v0.9.0.tgz | tar -xz -C $CNI_PATH
+    curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-arm64-v0.9.1.tgz | tar -xz -C $CNI_PATH
 fi
 
 mkdir -p /opt/cni
