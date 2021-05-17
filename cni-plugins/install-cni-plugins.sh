e5b5180043fc4888e10f583fca30d1e9caedbacf
Adam Bolsover <13262235+bowseruk@users.noreply.github.com>
2021-05-17 10:44:53 +0100

    Delete install-cni-plugins.sh

--- cni-plugins/install-cni-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-arm64-v0.9.1.tgz -o /tmp/cni.tgz
-mkdir -p /mnt/data/podman/cni/
-tar xf /tmp/cni.tgz -C /mnt/data/podman/cni/
-rm /tmp/cni.tgz
