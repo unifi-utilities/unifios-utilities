75629b6c5a85882f2cf2157d5e53ec6411fc1cd0
John D <john@boostchicken.dev>
2021-05-25 05:40:13 -0700

    Add podman update
    Former-commit-id: 6d68e290e02f39e8a93af8af2df74bf649408350

--- podman-update/00-podman-update.sh
+++ podman-update/00-podman-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p /mnt/data/podman-update
+mkdir -p /mnt/data/.cache
 
 PODMAN_DL=/mnt/data/.cache/podman
 CONMON_DL=/mnt/data/.cache/conmon
@@ -11,19 +11,19 @@ if [ ! -f CONMON_DL ]; then
 fi
 chmod +x $CONMON_DL
 mv /usr/libexec/podman/conmon /usr/libexec/podman/conmon.old
-
 ln -s $CONMON_DL /usr/libexec/podman/conmon
 
 if [ ! -f $PODMAN_DL ]; then
   curl -fsSLo $PODMAN_DL https://raw.githubusercontent.com/boostchicken/udm-utilities/master/podman-update/bin/podman
 fi
 chmod +x $PODMAN_DL
-# mv /usr/bin/podman /usr/bin/podman.old
-#ln -s $PODMAN_DL /usr/bin/podman
+mv /usr/bin/podman /usr/bin/podman.old
+ln -s $PODMAN_DL /usr/bin/podman
 
 if [ ! -f $SECCOMP ]; then
   mkdir -p /usr/share/container/
   curl -fsSLo /usr/share/container/seccomp.json https://raw.githubusercontent.com/boostchicken/udm-utilities/master/podman-update/bin/seccomp.json
 fi
-
+sed -i 's/driver = ""/driver = "overlay"/' /etc/containers/storage.conf
+# Comment out if you don't want to enable docker-compose or remote docker admin
 /usr/bin/podman system service --time=0 tcp:0.0.0.0:2375 &
