66ed81c9a3b697e6bd4348c8d39e5b7a0966c224
Jon Stroud <jon.stroud@keysight.com>
2021-06-17 10:32:12 -0500

    Add a script to update your pihole container

---
+++ run-pihole/upd_pihole.sh
@@ -0,0 +1,17 @@
+podman pull pihole/pihole:latest
+podman stop pihole
+podman rm pihole
+podman run -d --network dns --restart always \
+    --name pihole \
+    -e TZ="America/Chicago" \
+    -v "/mnt/data/etc-pihole/:/etc/pihole/" \
+    -v "/mnt/data/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    --dns=127.0.0.1 \
+    --dns=1.1.1.1 \
+    --dns=1.0.0.1 \
+    --hostname pi.hole \
+    -e VIRTUAL_HOST="pi.hole" \
+    -e PROXY_LOCATION="pi.hole" \
+    -e ServerIP="10.0.5.3" \
+    -e IPv6="False" \
+    pihole/pihole:latest
