44baea52a79276d36d6bbb39dd02b0bf9b626f6d
Larry Silverman <lsilverman@trackabout.com>
2021-07-03 00:35:37 -0500

    Fixes required for successful podman replacement (#198)
    * Fixes required for successful podman replacement
    
    * Missed a couple paths.

--- podman-update/01-podman-update.sh
+++ podman-update/01-podman-update.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /mnt/data/.cache
 
-PODMAN_VERSION=3.2.0
+PODMAN_VERSION=3.2.0-dev
 RUNC_VERSION=1.0.0-rc95
 CONMON_VERSION=2.0.27
 PODMAN_DL=/mnt/data/.cache/podman-$PODMAN_VERSION
 RUNC_DL=/mnt/data/.cache/runc-$RUNC_VERSION
 CONMON_DL=/mnt/data/.cache/conmon-$CONMON_VERSION
-SECCOMP=/usr/share/container/seccomp.json
+SECCOMP=/usr/share/containers/seccomp.json
 
 while [ ! -f $CONMON_DL ]; do
   curl -fsSLo $CONMON_DL https://github.com/containers/conmon/releases/download/v$CONMON_VERSION/conmon.arm64
@@ -39,8 +39,8 @@ fi
 ln -s $RUNC_DL /usr/bin/runc
 
 if [ ! -f $SECCOMP ]; then
-  mkdir -p /usr/share/container/
-  curl -fsSLo /usr/share/container/seccomp.json https://github.com/boostchicken/udm-utilities/blob/master/podman-update/bin/seccomp.json?raw=true
+  mkdir -p /usr/share/containers/
+  curl -fsSLo $SECCOMP https://github.com/boostchicken/udm-utilities/blob/master/podman-update/bin/seccomp.json?raw=true
 fi
 sed -i 's/driver = ""/driver = "overlay"/' /etc/containers/storage.conf
 # Comment out if you don't want to enable docker-compose or remote docker admin
