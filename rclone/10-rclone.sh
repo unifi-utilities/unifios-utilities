176f4dd6305af085f333da351a8bb9878a527f16
Christian Pearce <christian@pearcec.com>
2021-07-24 16:20:54 -0400

    Mount the /rclone directory to avoid a copy backup error.

--- rclone/10-rclone.sh
+++ rclone/10-rclone.sh
@@ -12,7 +12,7 @@ else
     -v /mnt/data/on_boot.d:/data/backups/on_boot.d \
     -v /data/unifi/data/backup/autobackup:/data/backups//data/unifi/data/backup/autobackup \
     -v /mnt/data/podman/cni:/data/backups/podman/cni \
-    -v /mnt/data/rclone/rclone.conf:/config/rclone/rclone.conf \
+    -v /mnt/data/rclone:/config/rclone \
     -v /mnt/data/rclone/sync.sh:/data/sync.sh \
     --name "$CONTAINER" \
     --security-opt=no-new-privileges \
