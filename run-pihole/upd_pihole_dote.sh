6af56672a229528faab05f4ae0dcbf83e8777e59
Chris Staite <chris@yourdreamnet.co.uk>
2021-10-18 20:25:57 +0100

    Add DoTe instructions to PiHole for DNS-over-TLS.

---
+++ run-pihole/upd_pihole_dote.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+podman pull pihole/pihole:latest
+podman build -t pihole:latest --format docker -f /dev/fd/0 <<EOF
+FROM pihole/pihole:latest
+ENV DOTE_OPTS="-s 127.0.0.1:5053"
+RUN echo -e  "#!/bin/sh\ncurl -fsSLo /opt/dote https://github.com/chrisstaite/DoTe/releases/latest/download/dote_arm64\nchmod +x /opt/dote\n/opt/dote \\\$DOTE_OPTS -d\n" > /etc/cont-init.d/10-dote.sh
+EOF
+podman stop pihole
+podman rm pihole
+podman run -d --network dns --restart always \
+    --name pihole \
+    -e TZ="America/Chicago" \
+    -v "/mnt/data/etc-pihole/:/etc/pihole/" \
+    -v "/mnt/data/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    --dns=127.0.0.1 \
+    --hostname pi.hole \
+    -e DOTE_OPTS="-s 127.0.0.1:5053 -m 10" \
+    -e VIRTUAL_HOST="pi.hole" \
+    -e PROXY_LOCATION="pi.hole" \
+    -e PIHOLE_DNS_="127.0.0.1#5053" \
+    -e ServerIP="10.0.5.3" \
+    -e IPv6="False" \
+    pihole:latest
+
