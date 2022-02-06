588d3495f84c8f6ab42b9dcc730bdff338ea5bef
buckmelanoma <43704682+buckmelanoma@users.noreply.github.com>
2022-02-06 01:14:18 -0800

    Add missing 'fi', fix indentation, fix comment (#322)
    Script doesn't run without fi on the last line.  Fixed cloudflare comment lines.

--- haproxy/50-haproxy.sh
+++ haproxy/50-haproxy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 CONTAINER=haproxy
 
-# Starts a cloudflare ddns container that is deleted after it is stopped.
-# All configs stored in /mnt/data/cloudflare-ddns
+# Starts an haproxy container that is deleted after it is stopped.
+# All configs stored in /mnt/data/haproxy
 if podman container exists "$CONTAINER"; then
   podman start "$CONTAINER"
 else
-podman run -d --net=host --restart always \
-  --name haproxy \
-  --hostname ha.proxy \
-  -v "/mnt/data/haproxy/:/usr/local/etc/haproxy/" \
-  haproxy:latest
+  podman run -d --net=host --restart always \
+    --name haproxy \
+    --hostname ha.proxy \
+    -v "/mnt/data/haproxy/:/usr/local/etc/haproxy/" \
+    haproxy:latest
+fi
