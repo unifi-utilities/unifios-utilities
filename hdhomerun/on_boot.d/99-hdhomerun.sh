381ec1e022777edec2bb60a0068b3f0d5cc81bf6
owine <owine@users.noreply.github.com>
2022-03-25 08:08:46 -0500

    hdhomerun: Correction in on_boot script (#339)
    I needed to make this edit in order for the script to work for me. The brackets were being passed to socat which was giving a name not known error.

--- hdhomerun/on_boot.d/99-hdhomerun.sh
+++ hdhomerun/on_boot.d/99-hdhomerun.sh
@@ -2,4 +2,4 @@
 # Place cross compiled version of `socat` in /mnt/data/hdhomerun
 HDHOMERUN_IP=10.10.30.146
 
-/mnt/data/hdhomerun/socat -d -d -v udp4-recvfrom:65001,broadcast,fork udp4-sendto:{$HDHOMERUN_IP}:65001 &
+/mnt/data/hdhomerun/socat -d -d -v udp4-recvfrom:65001,broadcast,fork udp4-sendto:$HDHOMERUN_IP:65001 &
