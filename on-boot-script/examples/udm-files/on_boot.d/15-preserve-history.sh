42f943db8ab5019e90cb55aca8cf060fec3ca48c
Bennett Perkins <bennettperkins@gmail.com>
2022-12-18 07:50:34 +0800

    preserve bash history too

--- on-boot-script/examples/udm-files/on_boot.d/15-preserve-history.sh
+++ on-boot-script/examples/udm-files/on_boot.d/15-preserve-history.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 mkdir -p /mnt/data/.home
-if [ ! -f /mnt/data/.home/.ash_history ]; then
-  cp /root/.ash_history /mnt/data/.home/.ash_history
+
+for file in .ash_history .bash_history
+if [ ! -f /mnt/data/.home/$file ]; then
+  touch /root/$file
+  cp /root/$file /mnt/data/.home/$file
+  chown root:root /mnt/data/.home/$file
+  chmod 0600 /mnt/data/.home/$file
 fi
-ln -sf /mnt/data/.home/.ash_history /root/.ash_history
+ln -sf /mnt/data/.home/$file /root/$file
+
