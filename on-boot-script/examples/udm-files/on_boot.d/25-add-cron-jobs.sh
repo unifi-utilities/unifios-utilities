8f7a48ee08af5cc7a88c3c12cf68cbf90208719b
bruvv <3063928+bruvv@users.noreply.github.com>
2023-02-22 13:23:45 +0100

    Corrected file path by removing /mnt

--- on-boot-script/examples/udm-files/on_boot.d/25-add-cron-jobs.sh
+++ on-boot-script/examples/udm-files/on_boot.d/25-add-cron-jobs.sh
@@ -3,7 +3,7 @@
 ## Store crontab files in /mnt/data/cronjobs/ (you will need to create this folder).
 ## This script will re-add them on startup.
 
-cp /mnt/data/cronjobs/* /etc/cron.d/
+cp /data/cronjobs/* /etc/cron.d/
 /etc/init.d/crond restart
 
 exit 0
