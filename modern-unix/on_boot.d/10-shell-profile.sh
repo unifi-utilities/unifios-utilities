unifi-utilities/unifios-utilities

commit e5269cf3cfb1951a8748162a1c461d2d9b55efce

4.0 support (#608)

modern-unix/on_boot.d/10-shell-profile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bssh
 
 ## Configure shell profile
 PROFILE_SOURCE=/data/settings/profile/global.profile.d

on-boot-script-2.x/examples/udm-files/on_boot.d/10-shell-profile.sh
@@ -5,10 +5,7 @@ case "$(ubnt-device-info firmware || true)" in
 1*)
     DATA_DIR="/mnt/data"
     ;;
-2*)
-    DATA_DIR="/data"
-    ;;
-3*)
+2* | 3* | 4*)
     DATA_DIR="/data"
     ;;
 *)

att-ipv6/10-att-ipv6.sh
@@ -1,22 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 set -eo pipefail
 
 # Get DataDir location
 DATA_DIR="/data"
 case "$(ubnt-device-info firmware || true)" in
 1*)
-  DATA_DIR="/mnt/data"
-  ;;
-2*)
-  DATA_DIR="/data"
-  ;;
-3*)
-  DATA_DIR="/data"
-  ;;
+    DATA_DIR="/mnt/data"
+    ;;
+2* | 3* | 4*)
+    DATA_DIR="/data"
+    ;;
 *)
-  echo "ERROR: No persistent storage found." 1>&2
-  exit 1
-  ;;
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
 esac
 # Check if the directory exists
 if [ ! -d "$DATA_DIR/att-ipv6" ]; then

cloudflare-ddns/on_boot.d/30-cloudflare-ddns.sh
@@ -4,18 +4,15 @@ CONTAINER=cloudflare-ddns
 DATA_DIR="/data"
 case "$(ubnt-device-info firmware || true)" in
 1*)
-  DATA_DIR="/mnt/data"
-  ;;
-2*)
-  DATA_DIR="/data"
-  ;;
-3*)
-  DATA_DIR="/data"
-  ;;
+    DATA_DIR="/mnt/data"
+    ;;
+2* | 3* | 4*)
+    DATA_DIR="/data"
+    ;;
 *)
-  echo "ERROR: No persistent storage found." 1>&2
-  exit 1
-  ;;
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
 esac
 # Check if the directory exists
 if [ ! -d "$DATA_DIR/cloudflare-ddns" ]; then

dns-common/on_boot.d/10-dns.sh
@@ -3,19 +3,16 @@
 DATA_DIR="/data"
 case "$(ubnt-device-info firmware || true)" in
 1*)
-  DATA_DIR="/mnt/data"
-  ;;
-2*)
-  DATA_DIR="/data"
-  ;;
-3*)
-  DATA_DIR="/data"
-  ;;
+    DATA_DIR="/mnt/data"
+    ;;
+2* | 3* | 4*)
+    DATA_DIR="/data"
+    ;;
 *)
-  echo "ERROR: No persistent storage found." 1>&2
-  exit 1
-  ;;
-esac
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
+esac 
 ## configuration variables:
 VLAN=5
 IPV4_IP="10.0.5.3"

haproxy/50-haproxy.sh
@@ -4,20 +4,16 @@ CONTAINER=haproxy
 DATA_DIR="/data"
 case "$(ubnt-device-info firmware || true)" in
 1*)
-  DATA_DIR="/mnt/data"
-  ;;
-2*)
-  DATA_DIR="/data"
-  ;;
-3*)
-  DATA_DIR="/data"
-  ;;
+    DATA_DIR="/mnt/data"
+    ;;
+2* | 3* | 4*)
+    DATA_DIR="/data"
+    ;;
 *)
-  echo "ERROR: No persistent storage found." 1>&2
-  exit 1
-  ;;
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
 esac
-
 # Check if the directory exists
 if [ ! -d "${DATA_DIR}/haproxy" ]; then
   # If it does not exist, create the directory

haproxy/update-haproxy.sh
@@ -3,20 +3,16 @@ IMAGE=haproxy:latest
 DATA_DIR="/data"
 case "$(ubnt-device-info firmware || true)" in
 1*)
-  DATA_DIR="/mnt/data"
-  ;;
-2*)
-  DATA_DIR="/data"
-  ;;
-3*)
-  DATA_DIR="/data"
-  ;;
+    DATA_DIR="/mnt/data"
+    ;;
+2* | 3* | 4*)
+    DATA_DIR="/data"
+    ;;
 *)
-  echo "ERROR: No persistent storage found." 1>&2
-  exit 1
-  ;;
+    echo "ERROR: No persistent storage found." 1>&2
+    exit 1
+    ;;
 esac
-
 podman pull $IMAGE
 podman stop haproxy
 podman rm haproxy

hdhomerun/on_boot.d/99-hdhomerun.sh
@@ -8,10 +8,7 @@ case "$(ubnt-device-info firmware || true)" in
 1*)
     DATA_DIR="/mnt/data"
     ;;
-2*)
-    DATA_DIR="/data"
-    ;;
-3*)
+2* | 3* | 4*)
     DATA_DIR="/data"
     ;;
 *)

commit d3e63faad1409d9bacacc7953e71232f6c246ebf

Fix typo 10-shell-profile.sh
Introduced in https://github.com/unifi-utilities/unifios-utilities/pull/608

modern-unix/on_boot.d/10-shell-profile.sh
@@ -1,4 +1,4 @@
-#!/bin/bssh
+#!/bin/bash
 
 ## Configure shell profile
 PROFILE_SOURCE=/data/settings/profile/global.profile.d