e432f7679263fadcbeafbd28b1900705d2561433
Varun Mehta <smartperson@gmail.com>
2025-01-13 09:04:56 -0500

    feat: add support for UniFi UXG Max (#631)

--- on-boot-script-2.x/remote_install.sh
+++ on-boot-script-2.x/remote_install.sh
@@ -75,6 +75,9 @@ udm_model() {
   "UniFi Dream Machine Pro Max")
     echo "udmpromax"
     ;;
+  "Gateway Max")
+    echo "uxgmax"
+    ;;
   *)
     echo "unknown"
     ;;
@@ -179,7 +182,7 @@ udmlegacy | udmprolegacy)
 
   echo "UDM Boot Script installed"
   ;;
-udr | udmse | udm | udmpro | udmpromax)
+udr | udmse | udm | udmpro | udmpromax | uxgmax)
   echo "$(ubnt-device-info model) version $(ubnt-device-info firmware) was detected"
   echo "Installing on-boot script..."
   depends_on systemctl
